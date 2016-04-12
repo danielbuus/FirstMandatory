8e8e689637db3b7226db6527b33d3f6462d61151
Daniel Buus Christiansen <danielbuus1@gmail.com>
2016-04-12 11:07:03 +0200

    initial

---
+++ AarhusDevCoop/obj/Release/Package/PackageTmp/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusDevCoop.ViewModels;
+
+namespace AarhusDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : ContactFormSurfaceController
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+    }
+}
